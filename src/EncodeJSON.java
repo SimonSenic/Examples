22a5151eec947cc1d94cd4e463888e32270eb2bf
Simon Senic <simon.senic@kosickaakademia.sk>
2021-01-27 10:07:43 +0100

    Added new method readPlaces()

--- src/EncodeJSON.java
+++ src/EncodeJSON.java
@@ -1,5 +1,9 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class EncodeJSON {
     public static void main(String[] args) {
@@ -10,5 +14,27 @@ public class EncodeJSON {
         //arr.add()
         obj.put("sport", "Football");
         System.out.println(obj);
+
+        readPlaces();
+    }
+
+    public static void readPlaces(){
+        JSONParser parser = new JSONParser();
+        try{
+            BufferedReader read = new BufferedReader(new FileReader("resources/places.json"));
+            JSONObject obj = (JSONObject)parser.parse(read);
+            JSONObject data = (JSONObject) obj.get("data");
+            for(int i=0; i<data.size(); i++){
+                JSONObject index = (JSONObject) data.get(String.valueOf(i));
+                String city = (String) index.get("city");
+                String region = (String) index.get("region");
+                String wikiDataId = (String) index.get("wikiDataId");
+                Double latitude = (Double) index.get("latitude");
+                Double longitude = (Double) index.get("longitude");
+                System.out.println("City: " +city +", Region: " +region +", WikiDataID: " +wikiDataId +", Latitude: " +latitude +", Longitude: " +longitude);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
